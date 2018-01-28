0d0de8d148b7c96ca13293d51f212f83be42aef5
Offir Golan <offirgolan@gmail.com>
2018-01-28 14:22:33 -0800

    Fix symlinks and asdf install

--- asdf/install.sh
+++ asdf/install.sh
@@ -8,11 +8,12 @@ asdf_dir=$HOME/.asdf
 
 if [ ! -d $asdf_dir ]; then
     echo "Installing asdf..."
-    git clone https://github.com/HashNuke/asdf.git $asdf_dir || exit 1
+    git clone https://github.com/asdf-vm/asdf.git $asdf_dir || exit 1
 
     # Install the correct dependencies
     if test "$(uname)" = "Darwin"
     then
+        echo "Installing asdf prerequisite dependencies..."
         brew install coreutils automake autoconf openssl libyaml readline libxslt libtool unixodbc
     fi
 
